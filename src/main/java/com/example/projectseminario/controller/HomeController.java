70bab14197e47201300a6d0e9d47ef5ef6dc7f8f
Miquéias Silva <d.miqueias@academico.ifg.edu.br>
2024-08-09 19:20:15 -0300

    Configuração thymeleaf

--- src/main/java/com/example/projectseminario/controller/HomeController.java
+++ src/main/java/com/example/projectseminario/controller/HomeController.java
@@ -1,2 +1,14 @@
-package com.example.projectseminario.controller;public class HomeController {
+package com.example.projectseminario.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
